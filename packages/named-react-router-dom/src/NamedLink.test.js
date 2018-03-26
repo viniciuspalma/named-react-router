f87b812a7ed13f26f933d5bd038fbd82dcfdbf06
Pedro S. Moreira <pedsmoreira@gmail.com>
2018-03-26 18:25:00 -0300

    Add NavLink test

--- packages/named-react-router-dom/src/NamedLink.test.js
+++ packages/named-react-router-dom/src/NamedLink.test.js
@@ -12,15 +12,20 @@ describe('<NamedLink />', () => {
 
   describe('#render', () => {
     it('returns a <Link /> with to', () => {
-      const wrapper = shallow(<NamedLink name="test" from="/from" />);
+      const wrapper = shallow(<NamedLink name="test" />);
       expect(wrapper.find('Link').length).toBe(1);
-      expect(wrapper.props()).toEqual({ to: '/test', from: '/from', replace: false });
+      expect(wrapper.props()).toEqual({ to: '/test', replace: false });
     });
 
     it('returns a <Link /> with to and other provided options', () => {
       const wrapper = shallow(<NamedLink name="test" from="/from" replace />);
       expect(wrapper.find('Link').length).toBe(1);
       expect(wrapper.props()).toEqual({ to: '/test', from: '/from', replace: true });
+    });
+
+    it('returns a <NavLink /> if navLink is true', () => {
+      const wrapper = shallow(<NamedLink name="test" navLink />);
+      expect(wrapper.find('NavLink').length).toBe(1);
     });
   });
 });
